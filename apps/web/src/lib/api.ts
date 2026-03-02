52bb3a71c16c581de3016ecc6a66e612d7cd0072
Wazap <gomaazarasi1789@gmail.com>
2026-03-02 23:16:56 +0900

    feat: FrontEndの環境構築 (#59)
    * feat: FrontEndの環境構築
    
    * feat: ディレクトリ構成の修正
    
    * feat: appsをルートディレクトリへ移動
    
    * feat: .gitignoreファイルを追加

---
+++ apps/web/src/lib/api.ts
@@ -0,0 +1,8 @@
+/**
+ * Backend APIクライアント
+ * バックエンドとの通信を行う関数を定義
+ */
+
+export const api = {
+  // TODO: Backend API呼び出しを実装
+};
---
+++ apps/web/src/lib/dify.ts
@@ -0,0 +1,8 @@
+/**
+ * Dify Chat API呼び出し
+ * Dify APIとの通信を行う関数を定義
+ */
+
+export const dify = {
+  // TODO: Dify Chat API呼び出しを実装
+};
